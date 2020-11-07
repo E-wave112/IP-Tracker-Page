828e1d7f1678cc75b5c4ed41295ad870d8c8eb3b
E-wave112 <osagieiyayi09@gmail.com>
2020-11-07 02:36:34 +0000

    protected jwt tokens

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // scripts
 const Form = document.form.input.value;
 const Btn = document.querySelector('.button');
+const Ip = document.querySelector('.ipad');
+const Locate = document.querySelector('.locat');
+const Time = document.querySelector('.time')
+const Int = document.querySelector('.internet');
+
 
 Btn.addEventListener('click', (e)=>{
     e.preventDefault();
@@ -8,11 +13,16 @@ Btn.addEventListener('click', (e)=>{
     //     alert('field cannot be empty');
     // }
 
-   const ipTrack =  fetch(`https://geo.ipify.org/api/v1?apiKey=at_IqOQwsXACnayytceHeXSz3C6JfHQf&ipAddress=${Form}`)
-    .then(response => {;
-        
-        console.log(typeof response);
-
+   fetch(`https://geo.ipify.org/api/v1?apiKey={{IPIFY_TOKEN}}&ipAddress=${Form}`)
+   .then(function(response){
+    return response.json()})
+.then(function(data){
+    console.log(data.location);
+    console.log(data.location.timezone);
+    Ip.textContent = Form;
+    Locate.textContent = data.location.region;
+    Time.textContent = 'UTC ' + data.location.timezone;
+    Int.textContent = data.isp;
     });
-    console.log(ipTrack)
+    
 });
