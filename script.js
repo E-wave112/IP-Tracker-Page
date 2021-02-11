df1e6cd9ea9cdefc8ab41cb9f11316c497610a99
E-wave112 <osagieiyayi09@gmail.com>
2021-02-11 20:56:28 +0000

    configured the mapping functionality

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ const Ip = document.querySelector('.ipad');
 const Locate = document.querySelector('.locat');
 const Time = document.querySelector('.time')
 const Int = document.querySelector('.internet');
+// Create the script tag, set the appropriate attributes
+
+      
 
 Btn.addEventListener('click', (e)=>{
     e.preventDefault();
@@ -29,6 +32,23 @@ Btn.addEventListener('click', (e)=>{
         Ip.textContent = data.ip;
         Locate.textContent = data.location.region;
         Time.textContent = 'UTC ' + data.location.timezone;
+        Int.textContent = data.isp;
+        var script = document.createElement('script');
+        script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDrq-3maSN2UxKEtfjUTOi-MPSKbb-x1Kk&callback=initMap';
+        script.async = true;
+        let map;
+        // Attach your callback function to the `window` object
+            window.initMap = function() {
+        // JS API is loaded and available
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: data.location.lat, lng: data.location.lng},
+                zoom: 8
+        });
+    };
+    // Append the 'script' element to 'head'
+    document.head.appendChild(script);
+
+
         // Int.textContent = data.isp;
         // var mymap = L.map('mapid',{
         //     center:[data.location.lat, data.location.lng],
